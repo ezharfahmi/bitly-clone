f713b03fa4fe39640d66a4cf98954f6baad53440
Next <nextacademy@email.com>
2015-11-26 17:35:55 +0700

    My First HTML Webpage

--- app/models/url.rb
+++ app/models/url.rb
@@ -2,7 +2,8 @@ require 'securerandom'
 
 class Url < ActiveRecord::Base
 	# This is Sinatra! Remember to create a migration!
-	
+	validates :long_url, presence: true
+	validates :long_url, format: { with: URI.regexp }, if: Proc.new { |a| a.long_url.present? }
 
 	before_create :shorten
 
